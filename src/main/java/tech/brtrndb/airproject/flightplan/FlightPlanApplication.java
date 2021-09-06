72b800852b885028c6f289d241ff6ed813ba5593
Bertrand B <boyer.bertrand@gmail.com>
2021-09-06 10:51:50 +0200

    Set up PostgreSQL and Flyway dependencies with configuration.

--- src/main/java/tech/brtrndb/airproject/flightplan/FlightPlanApplication.java
+++ src/main/java/tech/brtrndb/airproject/flightplan/FlightPlanApplication.java
@@ -2,8 +2,15 @@ package tech.brtrndb.airproject.flightplan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource(value = "classpath:/config/application.yml", ignoreResourceNotFound = false)
+})
+@EnableTransactionManagement
 public class FlightPlanApplication {
 
     public static void main(String[] args) {
